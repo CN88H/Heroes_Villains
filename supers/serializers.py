5cc1903516e19e9680c9fe585635d5942ac73e18
CN88H <cheenou88@gmail.com>
2022-06-20 02:25:17 -0500

    Registered Super model with admin sites + Completed Post Functionality for Super.

--- supers/serializers.py
+++ supers/serializers.py
@@ -4,4 +4,4 @@ from .models import Super
 class SuperSerializers(serializers.ModelSerializer):
     class Meta:
         model = Super
-        fields = ['id', 'name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_type']
+        fields = ['id', 'name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase']
--- supers/views.py
+++ supers/views.py
@@ -1,9 +1,19 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SuperSerializers
+from .models import Super
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def supers_list(request):
+    if request.method == 'GET':
+        supers = Super.objects.all()
+        serializer = SuperSerializers(supers, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SuperSerializers(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-
-    return Response('ok')
